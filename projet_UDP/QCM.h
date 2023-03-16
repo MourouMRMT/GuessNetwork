f4606779d13c141cb66f46172375c3e5ac8b4cb4
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-03-16 15:12:39 +0100

    Update QCM.h

--- projet_UDP/QCM.h
+++ projet_UDP/QCM.h
@@ -7,11 +7,11 @@ typedef struct {
 
 Question questions[NUM_QUESTIONS] = {
   {
-	"What is the capital of France? \n\t A:Paris \n\t B:Berlin \n\t C:Madrid \n\t D:London \n\t Choisissez votre reponse parmi A B C ou D \n\t",
+	"What is the capital of France? \n A:Paris \n B:Berlin \n C:Madrid \n D:London \n Choisissez votre reponse parmi A B C ou D \n",
     "A"
   },
   {
-    "What is the currency of Japan? \n\t A:Yen \n\t B:Dollar \n\t C:Euro \n\t D:Won \n\t Choisissez votre reponse parmi A B C ou D \n\t",
+    "What is the currency of Japan? \n A:Yen \n B:Dollar \n C:Euro \n D:Won \n Choisissez votre reponse parmi A B C ou D \n",
     "A"
   },
   // Add more questions here
