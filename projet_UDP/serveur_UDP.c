874c426d4d01cf2c0258e5ec0a625d07400b798e
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-03-16 16:30:30 +0100

    dssd

--- projet_UDP/serveur_UDP.c
+++ projet_UDP/serveur_UDP.c
@@ -120,8 +120,8 @@ void begin(char *chaine)
 {
     if(strcmp("oui",chaine)==0)
 	{
-		//strcpy(chaine,&questions[1].question);
-		strcpy(chaine,"oui");
+		strcpy(chaine,&questions[1].question);
+		//strcpy(chaine,"oui");
 		//flag=2;
 		//jeu(chaine);
 		if(strcmp("A",chaine)==0)
