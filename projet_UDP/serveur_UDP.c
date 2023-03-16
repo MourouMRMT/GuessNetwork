bd3862f7e86e8740a3da9fe31dd05621323e75af
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-03-16 15:17:35 +0100

    Update serveur_UDP.c

--- projet_UDP/serveur_UDP.c
+++ projet_UDP/serveur_UDP.c
@@ -112,6 +112,7 @@ void begin(char *chaine)
     if(strcmp("oui",chaine)==0)
 	{
 		strcpy(chaine,questions[0].question);
+		strcpy(chaine,"bite");
 		
 		if(strcmp(questions[0].correctOption,chaine)==0)
         {
