be53ba11004d595ad8dbce0883853026d36b18f3
VictorSdy <105672029+VictorSdy@users.noreply.github.com>
2023-03-21 11:37:59 +0100

    modif a faire encore

--- projet_UDP/QCM.h
+++ projet_UDP/QCM.h
@@ -14,5 +14,10 @@ Question questions[NUM_QUESTIONS] = {
     "What is the currency of Japan?\nA:Yen\n B:Dollar\nC:Euro\nD:Won\nChoisissez votre reponse parmi A B C ou D\n",
     'A'
   },
+  { “Who wrote the novel ‘1984’?\nA:George Orwell\n B:J.R.R. Tolkien\nC:Mark Twain\nD:Ernest Hemingway\nChoose your answer from A B C or D\n”, ‘A’ },
+  { “What is the largest bone in the human body?\nA:Tibia\n B:Femur\nC:Humerus\nD:Skull\nChoose your answer from A B C or D\n”, ‘B’ },
+  { “Which planet is the second from the Sun?\nA:Mars\n B:Venus\nC:Earth\nD:Mercury\nChoose your answer from A B C or D\n”, ‘B’ },
+  { “What is the name of the phobia that means fear of spiders?\nA:Arachnophobia\n B:Claustrophobia\nC:Agoraphobia\nD:Acrophobia\nChoose your answer from A B C or D\n”, ‘A’ },
+  { “Which country has the largest population in the world?\nA:India\n B:China\nC:USA \nD:Indonesia \nChoose your answer from A B C or D \n”, ‘B’ }
   // Add more questions here
 };
