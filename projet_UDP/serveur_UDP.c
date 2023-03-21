27dffa2904350b5fa5d2113dcca0f641150fb17f
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-03-21 11:16:24 +0100

    Update serveur_UDP.c

--- projet_UDP/serveur_UDP.c
+++ projet_UDP/serveur_UDP.c
@@ -131,7 +131,7 @@ void begin(char *chaine)
 
 		
 	}
-	printf("%s",questions[1].correctOption);
+	printf("%s",(char)questions[1].correctOption);
 	if(strcmp((char)questions[1].correctOption,chaine)==0)
 	{
 		strcpy(chaine,"GG tu as WIN!");
